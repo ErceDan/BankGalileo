2d946518cfa547f31a19b9bf841c342c12f6868c
ErceDan <daniel.erce.91@gmail.com>
2024-09-26 12:58:00 +0200

    commit inicial

---
+++ src/BankAccount.java
@@ -0,0 +1,18 @@
+public class BankAccount {
+    private double balance = 0.0;
+
+    public BankAccount(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+
+
+}
